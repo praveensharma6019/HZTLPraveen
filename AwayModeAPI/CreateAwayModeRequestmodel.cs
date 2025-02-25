5599ff37fe12898d6328aa2069cf3407fca806c1
praveensharma6019 <praveensharma6019@gmail.com>
2025-02-25 18:10:05 +0530

    Chnages For computed fields

--- AwayModeAPI/CreateAwayModeRequestmodel.cs
+++ AwayModeAPI/CreateAwayModeRequestmodel.cs
@@ -24,3 +24,45 @@ namespace DCX.Feature.EServices.AccountServices.Models.AwayMode
 
 
 
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace DCX.Feature.EServices.AccountServices.Models.AwayMode
+{
+    public class CreateAwayModeResponse
+    {
+        [JsonProperty("d")]
+        public CreateAwayModeResponseData CreateAwayModeResponseData { get; set; }
+    }
+    public class CreateAwayModeResponseData
+    {
+        [JsonProperty("PartnerNo")]
+        public string PartnerNo { get; set; }
+        [JsonProperty("AwayModeDetailsSet")]
+        public CreateAwayModeDetailsSet CreateAwayModeDetailsSet { get; set; }
+    }
+    public class CreateAwayModeDetailsSet
+    {
+        [JsonProperty("results")]
+        public List<CreateAwayModeDetailsSetResult> Results { get; set; }
+    }
+    public class CreateAwayModeDetailsSetResult
+    {
+        [JsonProperty("AwayModeID")]
+        public string AwayModeID { get; set; }
+        [JsonProperty("Alias")]
+        public string Alias { get; set; }
+        [JsonProperty("PartnerNo")]
+        public string PartnerNo { get; set; }
+        [JsonProperty("ContractAccount")]
+        public string ContractAccount { get; set; }
+        [JsonProperty("StartDate")]
+        public string StartDate { get; set; }
+        [JsonProperty("EndDate")]
+        public string EndDate { get; set; }
+        [JsonProperty("PushFrequency")]
+        public int PushFrequency { get; set; }
+    }
+}
